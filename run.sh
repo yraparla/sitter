02420a151b2618b93100c7a120e92b9e31805edd
vzvenyach <vdavez@gmail.com>
2014-11-14 07:20:28 -0600

    fixed the wget command to overwrite existing file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 source env/bin/activate
-wget www.cafc.uscourts.gov/argument/upcoming-oral-arguments.html
+wget www.cafc.uscourts.gov/argument/upcoming-oral-arguments.html -O upcoming-oral-arguments.html
 python sitter.py > sittings.json

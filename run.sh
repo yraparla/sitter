f3850b943fbdd34edd6b602e97d6c9afbdfe3d1c
Swi <yasaswi.raparla@gmailcom>
2015-10-27 20:54:50 +0000

    20151027

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 source env/bin/activate
 wget www.cafc.uscourts.gov/argument/upcoming-oral-arguments -O upcoming-oral-arguments.html
 python sitter.py > sittings.json
+python stats.py
 git commit -am $(date +%Y%m%d)
 git push origin master
